e40bae044c1d5282c550599f2c734d53f30895e4
Matthias Daelemans <circusmatthias@live.be>
2020-07-28 20:39:05 +0200

    Create donations.js

---
+++ donations.js
@@ -0,0 +1,20 @@
+const Twit = require('twit');
+const TwitterBot = require('node-twitterbot').TwitterBot;
+const fetch = require('node-fetch');
+
+// Write the Env variables on Heroku or your server, to keep them save from Github
+var Bot = new TwitterBot({
+        consumer_key: 'HoLOf2E80xQd9VjBSAsdnjlwe',
+        consumer_secret: '18Q5l361yHMVryutoJ7Oaup0lc3MObWwEHOzvWjTygtbupCkbm',
+        access_token: '1285958645830889479-uxjJAhTuy1BJnWzzw1fS5G9BrHZhjG',
+        access_token_secret: 'ik6Y1bXeTXd2fRj9yW5KQKvG2sZ4Nmc15OZr4S2Y7Ru9c'
+});
+
+// Function that builds and tweets the message
+function runBot() {
+        // Send Tweet
+        Bot.tweet(`Bitcoin donations: 3EjCzHqKr3hQYCbPoFTHLLAGfWgQTmk6oY`)
+}
+
+// run Bot
+runBot();
